161376fc05bc74662de4d1b3aa04335abcbb7c92
akarshika23 <akarshikacd2030@gmail.com>
2022-04-23 15:59:44 +0530

    my assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,8 +15,12 @@ const loginUser = async function (req, res) {
   let userName = req.body.emailId;
   let password = req.body.password;
 
+  console.log(userName,password);
+
   let user = await userModel.findOne({ emailId: userName, password: password });
+ 
   if (!user)
+
     return res.send({
       status: false,
       msg: "username or the password is not corerct",
@@ -57,8 +61,13 @@ const getUserData = async function (req, res) {
   let decodedToken = jwt.verify(token, "functionup-thorium");
   if (!decodedToken)
     return res.send({ status: false, msg: "token is invalid" });
+   if(decodedToken.userId!==req.params.userId){
+    return res.send("Token is valid but not of the user")
+   }
+   
 
   let userId = req.params.userId;
+  console.log(userId);
   let userDetails = await userModel.findById(userId);
   if (!userDetails)
     return res.send({ status: false, msg: "No such user exists" });
@@ -83,7 +92,12 @@ const updateUser = async function (req, res) {
   let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
   res.send({ status: updatedUser, data: updatedUser });
 };
-
+ const deleteUser = async function (req ,res){
+     let  userId = req.params.userId;
+     let deletedUser = await userModel.findOneAndUpdate({_id: userId}, {isDeleted:true},{new:true});
+     res.send({ status:"deletedUser", data:deletedUser});
+ }
+module.exports.deleteUser=deleteUser
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
