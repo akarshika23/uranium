785871bbf2944073984c27661a9297c8a81fe8ee
akarshika23 <akarshikacd2030@gmail.com>
2022-04-26 18:43:32 +0530

    my changes

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,85 +1,156 @@
-let axios = require("axios")
-
+let axios = require("axios");
 
 let getStates = async function (req, res) {
-
-    try {
-        let options = {
-            method: 'get',
-            url: 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
-
+  try {
+    let options = {
+      method: "get",
+      url: "https://cdn-api.co-vin.in/api/v2/admin/location/states",
+    };
+    let result = await axios(options);
+    console.log(result);
+    let data = result.data;
+    res.status(200).send({ msg: data, status: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
 
 let getDistricts = async function (req, res) {
-    try {
-        let id = req.params.stateId
-        let options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
-        }
-        let result = await axios(options);
-        console.log(result)
-        let data = result.data
-        res.status(200).send({ msg: data, status: true })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
+  try {
+    let id = req.params.stateId;
+    let options = {
+      method: "get",
+      url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`,
+    };
+    let result = await axios(options);
+    console.log(result);
+    let data = result.data;
+    res.status(200).send({ msg: data, status: true });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
 
 let getByPin = async function (req, res) {
-    try {
-        let pin = req.query.pincode
-        let date = req.query.date
-        console.log(`query params are: ${pin} ${date}`)
-        var options = {
-            method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
-        }
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
+  try {
+    let pin = req.query.pincode;
+    let date = req.query.date;
+    console.log(`query params are: ${pin} ${date}`);
+    var options = {
+      method: "get",
+      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`,
+    };
+    let result = await axios(options);
+    console.log(result.data);
+    res.status(200).send({ msg: result.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
 
 let getOtp = async function (req, res) {
-    try {
-        let blahhh = req.body
-        
-        console.log(`body is : ${blahhh} `)
-        var options = {
-            method: "post",
-            url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
-            data: blahhh
-        }
+  try {
+    let blahhh = req.body;
 
-        let result = await axios(options)
-        console.log(result.data)
-        res.status(200).send({ msg: result.data })
-    }
-    catch (err) {
-        console.log(err)
-        res.status(500).send({ msg: err.message })
-    }
-}
+    console.log(`body is : ${blahhh} `);
+    var options = {
+      method: "post",
+      url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
+      data: blahhh,
+    };
 
+    let result = await axios(options);
+    console.log(result.data);
+    res.status(200).send({ msg: result.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
+let get_District = async function (req, res) {
+  try {
+    let district_Id = req.query.district_id;
+    let date = req.query.date;
+    // console.log(`query params are: ${pin} ${date}`);
+    var options = {
+      method: "get",
+      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_Id}&date=${date}`,
+    };
+    let result = await axios(options);
+    console.log(result.data);
+    res.status(200).send({ msg: result.data });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
+let get_Weather = async function (req, res) {
+  try {
+    let country = req.query.country;
+    let appid = req.query.appid;
 
-module.exports.getStates = getStates
-module.exports.getDistricts = getDistricts
-module.exports.getByPin = getByPin
-module.exports.getOtp = getOtp
+    // console.log(`query params are: ${country} ${appId}`);
+    var options = {
+      method: "get",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${appid}`,
+    };
+    let result = await axios(options);
+    console.log(result.data.main.temp);
+    res.status(200).send({ msg: result.data.main.temp });
+  } catch (err) {
+    // console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
+let get_cities = async function (req, res) {
+  try {
+    let country = req.query.country;
+    let appid = req.query.appid;
+
+    // console.log(`query params are: ${country} ${appId}`);
+    var options = {
+      method: "get",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${country}&appid=${appid}`,
+    };
+    let result = await axios(options);
+    console.log(result.data.main.temp);
+    res.status(200).send({ msg: result.data.main.temp });
+  } catch (err) {
+    // console.log(err);
+    res.status(500).send({ msg: err.message });
+  }
+};
+const get_memes_request = async function (req, res) {
+  let template_id = req.query.template_id;
+  let text0 = req.query.text0;
+  let text1 = req.query.text1;
+  let username = req.query.username;
+  let password = req.query.password;
+  let data_body = req.body;
+  try {
+    var options = {
+      method: "post",
+      url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text=${text0}&text1=${text1}&username=${username}&password=${password}`,
+      data: data_body,
+    };
+
+    let result = await axios(options);
+    // console.log(`the values are ${result.data}`);
+    res.status(200).send({ data: result.data });
+  } catch (err) {
+    res.status(500).send({ msg: err.message });
+  }
+};
+
+module.exports.getStates = getStates;
+module.exports.get_District = get_District;
+module.exports.get_cities = get_cities;
+
+module.exports.getDistricts = getDistricts;
+module.exports.getByPin = getByPin;
+module.exports.getOtp = getOtp;
+module.exports.get_Weather = get_Weather;
+module.exports.get_memes_request = get_memes_request;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const CowinController= require("../controllers/cowinController")
-
-
+const CowinController = require("../controllers/cowinController");
 
 router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+  res.send("My first ever api!");
+});
 
+router.get("/cowin/states", CowinController.getStates);
+router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts);
+router.get("/cowin/getByPin", CowinController.getByPin);
 
-router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
-router.get("/cowin/getByPin", CowinController.getByPin)
-
-router.post("/cowin/getOtp", CowinController.getOtp)
-
-// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
-
-
+router.post("/cowin/getOtp", CowinController.getOtp);
+router.get("/cowin/findByDistrict", CowinController.get_District);
+router.get("/api/getWeather", CowinController.get_Weather);
+router.get("/api/getcities", CowinController.get_cities);
+// router.post("/getmemes", CowinController.get_memes);
+router.post("/get_memes", CowinController.get_memes_request);
+// WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date)
 
 module.exports = router;
